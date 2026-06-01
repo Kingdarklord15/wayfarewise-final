4d7f387142a6aa8b79bb1bd4b91908ec585b75a6
manyasinghal06 <manya.singhal06@gmail.com>
2026-06-02 00:18:47 +0530

    add auth routes

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,1 +1,9 @@
+from flask import Blueprint
 
+from controllers.auth_controller import login, signup
+
+
+auth_bp = Blueprint("auth", __name__)
+
+auth_bp.post("/signup")(signup)
+auth_bp.post("/login")(login)
